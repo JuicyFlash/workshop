dab51e564f30d753f1a057294eb95063c13b44da
juicyflash <tankedet@bk.ru>
2024-04-12 22:27:06 +0300

    Исправлены тесты

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,9 +1,9 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order, optional: true
-  validates :first_name, presence: { message: "Поле имя не заполнено" }
-  validates :last_name, presence: {  message: "Поле фамилия не не заполнено" }
-  validates :city, presence: {  message: "Поле город не не заполнено" }
-  validates :street, presence: { message: "Поле улица не не заполнено" }
-  validates :house, presence: { message: "Поле дом не не заполнено" }
-  validates :phone, presence: { message: "Поле телефон не не заполнено" }
+  validates_presence_of :first_name, message: "Поле имя не заполнено"
+  validates_presence_of :last_name, message: "Поле фамилия не заполнено"
+  validates_presence_of :city, message: "Поле город не заполнено"
+  validates_presence_of :street, message: "Поле улица не заполнено"
+  validates_presence_of :house, message: "Поле дом не заполнено"
+  validates_presence_of :phone, message: "Поле телефон не заполнено"
 end
