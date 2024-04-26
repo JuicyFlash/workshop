75c6a3c169a4f4b1d9fbfb8db0fc94f362d4c89f
juicyflash <tankedet@bk.ru>
2024-04-26 20:11:23 +0300

    Improved UI Stimulus test +

---
+++ app/javascript/sometest/test.js
@@ -0,0 +1,7 @@
+const elem = document.getElementById('my-test-btn')
+if (elem){
+        document.getElementById('my-test-btn').addEventListener('click', () => {
+                console.log("SomeSome")
+        })
+}
+
